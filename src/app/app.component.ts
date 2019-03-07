cf9db771409702cfb35b33ec772a0faad9baa54e
arthur38 <arthur.bonnel38@gmail.com>
2019-03-07 17:40:38 +0100

    Création d'une class Post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  posts = [
+
+export class Post {
+    title: String;
+    content: String;
+    loveIts: number;
+    created_at: Date;
+}
+
+export class AppComponent extends Post {
+  posts: Post[] = [
     {
       title: 'Mon premier Post',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       loveIts: 0,
-      created_at: Date
+      created_at: new Date()
     },
     {
-      title: 'Mon Deuxième Post',
+      title: 'Mon deuxième Post',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       loveIts: 0,
-      created_at: Date
+      created_at: new Date()
     },
     {
-      title: 'Mon Troisième Post',
+      title: 'Encore un post',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       loveIts: 0,
-      created_at: Date
+      created_at: new Date()
     }
   ];
 }
